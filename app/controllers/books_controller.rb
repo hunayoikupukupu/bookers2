79395ffb2d684977763941d8a89f97ab29a3731b
EC2 Default User <ec2-user@ip-172-31-25-89.ap-northeast-1.compute.internal>
2023-06-13 03:17:13 +0000

    [Update] バリデーションの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,14 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @user = User.find(current_user.id)
+      @book_new = Book.new
+      @books = Book.all
+      render :index
+    end
 
   end
 
@@ -21,6 +27,16 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @book_new = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
-  
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
@@ -14,4 +14,8 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
+
+  validates :name, uniqueness: true
+  validates :name, length: {minimum: 2, maximum: 20 }
+  validates :introduction, length: {minimum: 0, maximum: 50 }
 end
